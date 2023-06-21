a88a04cc341595d739afa566c59f12c566faac8d
YiTiaoChangFen <liangbinz1599043@gmail.com>
2023-06-20 23:40:32 -0500

    fixed all bug, tests pased

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,13 +1,14 @@
 const removeFromArray = function(array) {
     for (let index = 0; index < array.length; index++) {
-        for (let i = 1; index < arguments.length; i++) {
-            if (array[index] == arguments[i]) {
+        for (let i = 1; i < arguments.length; i++) {
+            if (array[index] === arguments[i]) {
                 array.splice(index, 1);
+                --index;
             } else {
                 continue;
             }
         }
-        
+            
     }
     return array;
 };
