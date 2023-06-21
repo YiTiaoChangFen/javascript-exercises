55cd183af4826352e3b5c07c7070966b80a8048b
YiTiaoChangFen <liangbinz1599043@gmail.com>
2023-06-20 22:56:44 -0500

    basic coding done, first test passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array) {
+    for (let index = 0; index < array.length; index++) {
+        for (let i = 1; index < arguments.length; i++) {
+            if (array[index] == arguments[i]) {
+                array.splice(index, 1);
+            } else {
+                continue;
+            }
+        }
+        
+    }
+    return array;
 };
 
 // Do not edit below this line
