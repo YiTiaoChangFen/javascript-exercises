afa57c44e72f985acb945d2e1ac24808b82695d7
YiTiaoChangFen <liangbinz1599043@gmail.com>
2023-06-23 09:45:19 -0500

    finished funtion

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,27 @@
-const sumAll = function() {
+const sumAll = function(numOne, numTwo) {
+    let result = 0;
+    let firstDigit = numOne;
+    let lastDigit= numTwo;
+    let firstDigitType = typeof numOne;
+    let lastDigitType = typeof numTwo;
+
+    // error handling
+    if (numOne < 0 || numTwo < 0) {
+        return "ERROR";
+    } else if (firstDigitType != 'number' || lastDigitType != 'number') {
+        return "ERROR";
+    }
+
+    if (numOne > numTwo) {
+        firstDigit = numTwo;
+        lastDigit = numOne;
+    }
+
+    for (let index = firstDigit; index <= lastDigit; index++) {
+        result = result + index;
+    }
+
+    return result;
 
 };
 
