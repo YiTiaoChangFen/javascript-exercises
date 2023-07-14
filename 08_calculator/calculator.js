c5846df4503d65614f59830950690488ae4b0551
YiTiaoChangFen <liangbinz1599043@gmail.com>
2023-07-14 03:10:06 -0500

    all test passed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let result = 0;
+  for (let num in array) {
+    result = result + array[num];
+  }
+	return result;
 };
 
 const multiply = function() {
+  let multipliers = Array.from(arguments);
 
+  let result = multipliers[0]
+  for (let index = 1; index < multipliers.length; index++) {
+    result = result * multipliers[index];
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+  let result = base;
+
+  for (let index = 1; index < power; index++) {
+    result = result * base;
+  }
+	return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(fac) {
+	let result = 1;
+  for (let index = fac; index > 0; index--) {
+    result = result * index;
+  }
+  return result;
 };
 
 // Do not edit below this line
