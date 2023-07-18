72364ff03e4cd25b33d7f433e690d014fe5e4ee0
YiTiaoChangFen <liangbinz1599043@gmail.com>
2023-07-18 03:08:21 -0500

    finished exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let arr = [];
+    let book1 = books[0].title;
+    arr.push(book1)
+    let book2 = books[1].title;
+    arr.push(book2)
+    return arr;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    const currYear = 2023;
+    let oldestPerson = null;
+    let oldestAge = 0;
 
+    for (let people of array) {
+        let peopleAge = 0;
+        if ("yearOfDeath" in people) {
+            peopleAge = people.yearOfDeath - people.yearOfBirth;
+        } else {
+            peopleAge = currYear - people.yearOfBirth;
+        }
+        
+        if (peopleAge > oldestAge) {
+            oldestAge = peopleAge;
+            oldestPerson = people;
+        }
+    }
+    return oldestPerson;
 };
 
 // Do not edit below this line
