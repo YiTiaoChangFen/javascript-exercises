2b3a0fb0fa7253956239c5ef84aca4975f3247cc
YiTiaoChangFen <liangbinz1599043@gmail.com>
2023-07-17 23:27:34 -0500

    finished task

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(fibNum) {
+    let firstFib = 1;
+    let secondFib = 1;
+    let result = firstFib;
+
+    if (fibNum == 1 || fibNum == 2) {
+        return firstFib;
+    } else if (fibNum < 0) {
+        return "OOPS"
+    }
+
+    for (let index = 0; index < fibNum - 2; index++) {
+        result = firstFib + secondFib;
+        secondFib = firstFib;
+        firstFib = result;
+    }
+    return result;
 
 };
 
